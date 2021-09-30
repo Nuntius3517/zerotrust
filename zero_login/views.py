796734f6aa3a3bfabde24f2273c40d0ea5d9e25a
acs107128 <acs107128@gm.ntcu.edu.tw>
2021-09-30 15:36:32 +0800

    zerotrust

---
+++ zero_login/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse 
+from django.shortcuts import render
+
+
+def auth(request):
+    if request.user.is_authenticated:
+        return HttpResponse(status=200)
+    else:
+        return HttpResponse(status=401)
+
+
+def home_page(request):
+    return render(request,'home_page.html')
+
+
+
+
